047245f66a9c97336675b720fcd148c7160ad853
Override Development <OverrideDeveloper@protonmail.com>
2026-09-16 13:24:25 -0800

    Added GPL-3.0 licensure

--- web_server.lua
+++ web_server.lua
@@ -1,3 +1,23 @@
+-- Alice Web AI
+--
+-- Copyright (C) 2026 Override Development
+--
+-- This file is part of Alice Web AI.
+--
+-- Alice Web AI is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Alice Web AI is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Alice Web AI. If not, see
+-- <https://www.gnu.org/licenses/>.
+
 -- web_server.lua
 -- Luvit HTTP server for Alice Web AI.
 -- Lua 5.1 compatible.
